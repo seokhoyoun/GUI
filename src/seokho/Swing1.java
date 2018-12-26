128e9f0b9b3fd8b215efb4dec581c4521385e86d
Seokho Youn <axlover@hotmail.com>
2018-12-26 13:36:23 +0900

    init

---
+++ src/seokho/Swing1.java
@@ -0,0 +1,13 @@
+package seokho;
+
+import javax.swing.JFrame;
+
+public class Swing1 {
+
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		f.setSize(300, 200);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
+}
